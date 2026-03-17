6bc146442ea1becba2dc157ef7f0feaf7d44d98a
Parthrajsinh Sarvaiya <sarvaiyaparthrajsinh1818@gmail.com>
2026-03-17 16:35:28 +0530

    Add files via upload

---
+++ 13_string/1_char.c
@@ -0,0 +1,37 @@
+#include<stdio.h> 
+
+
+int main()
+
+{
+char letter = 'A';
+
+printf("%c\n", letter);
+printf("%d\n", letter);
+
+char samll = 'p';
+
+printf("%c\n", samll);
+printf("%d\n", samll);
+
+char capital = 's';
+
+printf("%c\n", capital);
+printf("%d\n",capital);
+
+
+char samllletter = 'd';
+
+printf("%c\n", samllletter);
+printf("%d\n",samllletter);
+
+
+ 
+
+
+
+
+
+
+    return 0;
+}
