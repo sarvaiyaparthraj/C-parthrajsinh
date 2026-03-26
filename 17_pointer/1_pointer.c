49741a0532482e7bd791cd62272c5a8000108270
Parthrajsinh Sarvaiya <sarvaiyaparthrajsinh1818@gmail.com>
2026-03-26 14:24:26 +0530

    Add files via upload

---
+++ 17_pointer/1_pointer.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main()
+
+{
+
+    int *ptr;
+
+
+    int a = 8;
+
+    ptr = &a;
+
+    printf("Address is : % u", ptr);
+    
+
+    return 0;
+}
---
+++ 17_pointer/2_sizeof.c
@@ -0,0 +1,22 @@
+
+
+#include<stdio.h>
+
+int main()
+
+{
+
+    int a=10;
+
+
+    printf("Size in bytes : %zu",sizeof(a));
+
+
+
+    printf("\nSize in bytes : %d",sizeof(a));
+
+
+
+
+    return 0;
+}
